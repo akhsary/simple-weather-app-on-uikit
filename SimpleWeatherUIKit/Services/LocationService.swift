e5a326b764ad0a643feeca3afbc51a696d9c26f8
Yuriy Chekan <chekanyr@icloud.com>
2024-05-19 21:00:02 +0300

    Location Service

--- SimpleWeatherUIKit/Services/LocationService.swift
+++ SimpleWeatherUIKit/Services/LocationService.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import CoreLocation
+final  class LocationService: NSObject, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager?
+    var location: CLLocationCoordinate2D?
+    var isLoading = false
+    static let shared = LocationService()
+    
+    func checkIfLocationIsEnabled() {
+        if CLLocationManager.locationServicesEnabled(){
+            self.locationManager = CLLocationManager()
+            self.locationManager!.delegate = self
+            self.checkLocationAytarization()
+        }  else {
+            print("some View will appear later")
+        }
+    }
+    
+    private func checkLocationAytarization() {
+        guard let locationManager = locationManager else { return }
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("some View will appear later")
+        case .denied:
+            print("some View will appear later")
+        case .authorizedAlways, .authorizedWhenInUse:
+            loadLocation(locationManager)
+        @unknown default:
+            break
+        }
+    }
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAytarization()
+    }
+    func loadLocation(_ locationManager: CLLocationManager) {
+        isLoading = true
+        location = locationManager.location?.coordinate
+        isLoading = false
+    }
+    private init(locationManager: CLLocationManager? = nil, location: CLLocationCoordinate2D? = nil, isLoading: Bool = false) {
+        self.locationManager = locationManager
+        self.location = location
+        self.isLoading = isLoading
+    }
+}
+
